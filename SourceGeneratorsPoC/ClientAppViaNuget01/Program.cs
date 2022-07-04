c5e5b912faec551597c1a4ec9934d760402b5d8b
macrogreg <macrogreg@live.com>
2022-07-04 16:41:17 -0700

    Create Src Gen Nuget and add empty Nuget test project.

---
+++ SourceGeneratorsPoC/ClientAppViaNuget01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Temporal.Prototypes.ClientAppViaNuget01
+{
+    public class Program
+    {
+        public static void Main(string[] _)
+        {
+            (new Program()).Run();
+        }
+
+        public void Run()
+        {
+            Console.WriteLine($"\"{this.GetType().FullName}\" was run.");
+
+            
+
+            Console.WriteLine($"\"{this.GetType().FullName}\" was stopped.");
+        }
+    }
+}
